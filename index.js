dcc3c6e64e10dc80d84acaa5412264965b89999f
Joseph Nelly Sugu Sugira <josephnellysugusugira@josephs-mbp.myfiosgateway.com>
2020-12-19 23:54:44 -0500

    sets up for monolithic app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 4000;
+const server = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static("front-end/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+server.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./front-end/build/index.html"));
+});
+
+server.listen(PORT, function () {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
